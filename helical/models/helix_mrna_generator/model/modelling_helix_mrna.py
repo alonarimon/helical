165755d86f18c62c062450d77db1a670e6c7d927
alonarimon <rimonalona@gmail.com>
2025-07-22 18:04:32 +0000

    modelling_helix_mrna.py : update to Matt changes - fix hidden state

--- helical/models/helix_mrna_generator/model/modelling_helix_mrna.py
+++ helical/models/helix_mrna_generator/model/modelling_helix_mrna.py
@@ -1655,6 +1655,13 @@ class HelixmRNAPretrainedModel(HelixmRNAPreTrainedModel):
                 )
 
             hidden_states = layer_outputs[0]
+            if output_hidden_states:
+                all_hidden_states += (layer_outputs[0],)
+
+        hidden_states = self.norm_f(layer_outputs[0])
+
+        if output_hidden_states:
+            all_hidden_states = all_hidden_states + (hidden_states,)
 
             if output_attentions:
                 if layer_outputs[1] is not None:
@@ -1663,11 +1670,6 @@ class HelixmRNAPretrainedModel(HelixmRNAPreTrainedModel):
 
         if use_cache:
             cache_params.seqlen_offset += inputs_embeds.shape[1]
-
-        hidden_states = self.norm_f(hidden_states)
-
-        if output_hidden_states:
-            all_hidden_states += (hidden_states,)
 
         if not return_dict:
             return tuple(
